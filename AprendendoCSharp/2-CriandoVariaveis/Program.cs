d433438333ab573cf1e6d5d884c2700850c0a2f5
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-04-24 15:12:32 -0300

    terceiro commit

--- AprendendoCSharp/2-CriandoVariaveis/Program.cs
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -8,6 +8,25 @@ namespace _2_CriandoVariaveis
         {
             Console.WriteLine("Executando Projeto 2, criando variaveis");
 
+            int idade;
+            idade = 26;
+
+            Console.WriteLine(idade);
+
+            idade = 10 + 5;
+
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+
+            Console.WriteLine(idade);
+
+            idade = 10 + 5 * 2;
+
+            Console.Write(idade);
+
+            Console.WriteLine("sua idade é " + idade + "!");
+
             Console.WriteLine("Execução finalizada. Tecle enter para sair");
             Console.ReadLine();
         }
