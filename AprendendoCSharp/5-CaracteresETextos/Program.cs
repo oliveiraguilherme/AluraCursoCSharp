e5ef66ac5c1a9ac8d54c6832696bfed98cb6570e
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-04-24 17:03:09 -0300

    sexto commit

---
+++ AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando O Projeto 4 Caracteres e Textos");
+
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de tecnologia " + 2020;
+            string cursosProgramacao = @"- .NET
+- Java
+- JavaScript";
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            Console.WriteLine("Execução finalizada tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AprendendoCSharp/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6 Atribuicoes de Variaveis");
+
+            int idade = 36;
+
+            int idadeGuilherme = idade;
+
+            idade = 20; 
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGuilherme);
+
+            Console.WriteLine("Execução finalizada, tecle enter para sair...");
+            Console.ReadLine();
+
+        }
+    }
+}
