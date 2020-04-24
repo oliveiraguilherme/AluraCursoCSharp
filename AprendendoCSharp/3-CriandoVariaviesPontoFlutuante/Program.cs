901f256679fe45ea1398485782641af7289172b0
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-04-24 15:30:38 -0300

    quarto commit

---
+++ AprendendoCSharp/3-CriandoVariaviesPontoFlutuante/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _3_CriandoVariaviesPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 3 criando variaveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15;
+
+            idade = idade / 2.0;
+
+            Console.WriteLine(idade);
+
+            idade = 5 / 3;
+            Console.WriteLine("5 / 3 = " + idade);
+
+            idade = 5.0 / 3.0;
+            Console.WriteLine("5 / 3 = " + idade);
+
+
+            Console.WriteLine("Execução finalizada, tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
