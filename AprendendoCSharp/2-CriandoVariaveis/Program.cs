76603f7e42b9204a3b30856340ad5f7fdc21eaa0
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-04-24 15:03:52 -0300

    segundo commit

---
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 2, criando variaveis");
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
