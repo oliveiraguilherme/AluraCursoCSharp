e5e665f548a0fcd88f1cda63f87bec664f4c7a5a
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-04-28 13:26:56 -0300

    trigésimo terceiro commit

---
+++ ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            contaDoBruno.titular = "Bruno";
+            Console.WriteLine(contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+            Console.ReadLine();
+        }
+    }
+}
