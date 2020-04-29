13af1056ac6a24305fd181644aac1e135190371c
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-04-29 18:00:08 -0300

    quinquagésimo nono commit

--- BB/ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ BB/ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -4,14 +4,14 @@ using System.Text;
 
 namespace ByteBank.Funcionarios
 {
-    public class Funcionario
+    public class Diretor
     {
         public string Nome { get; set; }
         public string CPF { get; set; }
         public double Salario { get; set; }
         public double GetBonificacao()
         {
-            return this.Salario * 0.1;
+            return this.Salario;
         }
     }
 }
---
+++ BB/ByteBank/ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+        public void Registrar(Diretor diretor)
+        {
+            _totalBonificacao += diretor.GetBonificacao();
+        }
+
+        public double GetBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
