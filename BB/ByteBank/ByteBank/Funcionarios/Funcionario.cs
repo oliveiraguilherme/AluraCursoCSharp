67948448704f5019d650bf6907146ba65e559057
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-04-29 17:43:04 -0300

    quinquagésimo oitavo commit

---
+++ BB/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        // 0 - funcionario
+        // 1 - diretor
+        // 2 - designer
+        // N - ...
+        private int _tipo;
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+            {
+                return Salario;
+            }
+            return this.Salario * 0.1;
+        }
+    }
+}
