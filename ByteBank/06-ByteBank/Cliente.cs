abc8d8131d41e5c365d4b4513ddfa3c83501fba0
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-04-29 13:59:20 -0300

    quadragésimo nono commit

--- ByteBank/06-ByteBank/Cliente.cs
+++ ByteBank/06-ByteBank/Cliente.cs
@@ -6,8 +6,23 @@ namespace _06_ByteBank
 {
     public class Cliente
     {
-        public string nome;
-        public string cpf;
-        public string profissao;
+        private string _cpf;
+
+        public string Nome { get; set; }
+        public string CPF
+        {
+            get
+            {
+                return _cpf;
+
+            }
+            set
+            {
+                //escrevo a minha logica aqui
+                _cpf = value;
+            }
+        }
+        public string Profissao { get; set; }
+       
     }
 }
--- ByteBank/06-ByteBank/Program.cs
+++ ByteBank/06-ByteBank/Program.cs
@@ -9,14 +9,14 @@ namespace _06_ByteBank
             ContaCorrente conta = new ContaCorrente();
             Cliente cliente = new Cliente();
 
-            cliente.nome = "Guilherme";
-            cliente.cpf = "434.564.879.20";
-            cliente.profissao = "Desenvolvedor";
+            cliente.Nome = "Guilherme";
+            cliente.CPF = "434.564.879.20";
+            cliente.Profissao = "Desenvolvedor";
 
             conta.Titular = cliente;
 
             conta.Saldo = -10;
-            Console.WriteLine(conta.Titular.nome);
+            Console.WriteLine(conta.Titular.Nome);
             Console.WriteLine(conta.Saldo);
 
             Console.ReadLine();
