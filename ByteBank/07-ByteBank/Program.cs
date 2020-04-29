f8599e73db8821cb15d9e328659e187b3906c50d
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-04-29 16:26:42 -0300

    quinquagésimo terceiro commit

---
+++ ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 96712540);
+
+           
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
