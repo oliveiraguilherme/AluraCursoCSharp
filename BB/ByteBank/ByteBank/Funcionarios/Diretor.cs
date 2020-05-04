7b2e138a46ef56de0c74d2a8bc9096fa7a809f1a
Guilherme Oliveira da Silva <guilherme.dasilva@radixeng.com.br>
2020-05-04 18:33:20 -0300

    septuagésimo quarto commit

--- BB/ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ BB/ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -5,11 +5,15 @@ using System.Text;
 
 namespace ByteBank.Funcionarios
 {
-    public class Diretor : Autenticavel
+    public class Diretor : FuncionarioAutenticavel
     {   
         public Diretor(string cpf) : base(5000, cpf)
         {
             Console.WriteLine("Criando DIRETOR");
+        }
+        public bool Autenticar(string senha)
+        {
+            return true;
         }
         public override void AumentarSalario()
         {
--- BB/ByteBank/ByteBank/Program.cs
+++ BB/ByteBank/ByteBank/Program.cs
@@ -27,7 +27,10 @@ namespace ByteBank
             camila.Nome = "Camila";
             camila.Senha = "abc";
 
-            
+            ParceiroComercial parceiro = new ParceiroComercial();
+            parceiro.Senha = "123456";
+
+            sistemaInterno.Logar(parceiro, "123456");
 
             sistemaInterno.Logar(roberta, "123");
             sistemaInterno.Logar(camila, "abc");
